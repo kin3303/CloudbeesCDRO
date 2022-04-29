1644b2bbe4ebcffa813d1f204a3461098e16846e
kin3303 <48729027+kin3303@users.noreply.github.com>
2022-04-29 13:17:24 +0900

    Update install_cd_server_centos.sh

--- data/server/install_cd_server_centos.sh
+++ data/server/install_cd_server_centos.sh
@@ -5,11 +5,25 @@ export LANGUAGE=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LC_CTYPE=en_US.UTF-8
- 
+
+RUN_USER=cbcd 
+RUN_GROUP=cbcd
+RUN_UID=2003
+RUN_GID=2003
+HOME_DIR=/home/${RUN_USER}
+INSTALL_DIR=/opt/cloudbees/sda
+  
+sudo rm -rf ${HOME_DIR} && \ 
+sudo rm -rf ${INSTALL_DIR} && \
+sudo mkdir -p ${HOME_DIR}
+
 sudo ln -sf /usr/share/zoneinfo/Asia/Seoul /etc/localtime
 
-serverUser="ubuntu"
-serverGroup="ubuntu"
+# user & group
+sudo groupadd --gid ${RUN_GID} ${RUN_GROUP} && \
+sudo useradd --uid ${RUN_UID} --gid ${RUN_GID} --home-dir ${HOME_DIR} --shell /bin/bash ${RUN_USER}
+serverUser="${RUN_USER}"
+serverGroup="${RUN_GROUP}"
 
 
 #download installer
@@ -17,25 +31,6 @@ sudo yum update -y
 sudo yum -y install wget  
 sudo wget https://downloads.cloudbees.com/cloudbees-cd/Release_10.4/10.4.2.153852/linux/CloudBeesFlow-x64-10.4.2.153852
 flowInstaller="CloudBeesFlow-x64-10.4.2.153852"
-
-#check user & group exist
-EXIST_USER=0
-UIDS=$(getent passwd | cut -d: -f1)
-for user in ${UIDS}
-do
-   group=$(getent group ${user} | cut -d: -f1)
-   if [[ "${user}" = "${serverUser}" && "${group}" = "${serverGroup}" ]]
-   then
-      EXIST_USER=1
-   fi
-done
-
-
-if [[ ${EXIST_USER} -eq 0 ]]
-then
-   echo "Please check if the user you entered is a valid user."
-   exit 1;
-fi
 
 #install cd server & web server
 sudo chmod +x ./${flowInstaller}
@@ -63,4 +58,8 @@ fi
 sudo chmod +x mysql-connector-java-8.0.27_ubuntu_20.04.jar
 sudo cp ./mysql-connector-java-8.0.27_ubuntu_20.04.jar /opt/cloudbees/sda/server/lib/mysql-connector-java.jar
 
-/etc/init.d/commanderServer restart
+sudo chmod -R "u=rwX,g=rX,o=rX" ${INSTALL_DIR}/ && \ 
+sudo chown -R ${RUN_USER}:${RUN_GROUP} ${INSTALL_DIR}/ && \
+sudo chown -R ${RUN_USER}:${RUN_GROUP} ${HOME_DIR} 
+
+sudo /etc/init.d/commanderServer restart
